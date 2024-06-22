498c474210d3cccd880969da185b8acfb1fb98b1
Ashriya26 <109788229+Ashriya26@users.noreply.github.com>
2024-06-22 19:36:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToDoList from './ToDoList'; // Ensure correct import
+
+function App() {
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
